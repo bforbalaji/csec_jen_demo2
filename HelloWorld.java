86650269589ce9181dd98cb3d27614603e3a2556
Balaji <71812832+bforbalaji@users.noreply.github.com>
2025-09-24 09:11:43 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Jenkins!");
+        System.out.println("This program was executed automatically.");
+        System.out.println("Current time: " + new java.util.Date());
+        
+        // Simple addition to show functionality
+        int a = 5;
+        int b = 3;
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
